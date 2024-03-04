add7d6be8227e8c1739cee0e264b2a360a5b87dd
devTatendaMarvelous <marveloustchimusoro@gmail.com>
2024-03-04 12:00:48 +0200

    Salary generation

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use App\Models\Salary;
 use App\Http\Requests\StoreSalaryRequest;
 use App\Http\Requests\UpdateSalaryRequest;
+use App\Models\TaskPayment;
 use Brian2694\Toastr\Facades\Toastr;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 class SalaryController extends Controller
 {
@@ -47,9 +51,55 @@ class SalaryController extends Controller
      */
     public function generate()
     {
-        Artisan::call('salaries' );
-        Toastr::success('Salaries Generated successfully', 'success');
-        return redirect('salaries');
+        try {
+            info('processing salaries...');
+            $employees = Employee::whereHas('user', function ($query) {
+                $query->where('status', 'active');
+            })->get();
+
+            $employees->each(function ($employee){
+                DB::beginTransaction();
+                $unpaidSalaryTasks = $employee->tasks->map(function ($task) {
+                    return $task->salaryTask()->where('is_paid', 0)->first();
+                })->filter();
+                $bonus=0;
+                $deductions=0;
+                $grosssalary=0;
+                $taskIds=[];
+                foreach($unpaidSalaryTasks as $task){
+                    $bonus+=$bonus;
+                    $deductions+=$deductions;
+                    $grosssalary+=($task->reward-$task->deductions+$task->bonus);
+                    $task->update(['is_paid'=>1]);
+                    $taskIds[]=$task->id;
+                }
+                if(count($unpaidSalaryTasks)){
+                    $salary=Salary::create([
+                        'employee_id'=>$employee->id,
+                        'month'=>Carbon::parse(now())->format('M'),
+                        'gross_salary'=>$grosssalary,
+                        'deductions'=>$deductions,
+                        'bonuses'=>$bonus,
+                        'net_salary'=>($grosssalary-$deductions+$bonus)
+                    ]);
+                    foreach ($taskIds as $taskId){
+                        TaskPayment::create([
+                            'salary_id'=>$salary->id,
+                            'salary_task_id'=>$taskId
+                        ]);
+                    }
+                }
+
+                DB::commit();
+            });
+
+            Toastr::success('Salaries Generated successfully', 'success');
+            return redirect('salaries');
+        }catch (\Exception $e){
+            DB::rollBack();
+            dd($e->getMessage());
+        }
+
     }
 
     /**
